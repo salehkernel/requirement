a9729a0cc90d0e232bb19bceb645e83468d7fcc3
MHD <mt0132@gmail.com>
2021-10-16 08:44:31 +0330

    remove filter elements. reformat the RequirementsController code. add private recursive method to update a treeItem. add public method to update and refresh the whole requirements treeView. change the way handling double mouse click on tree item. change the tree view selection mode to multiple selection. add the feature of creating new requirement with parents which are selected from tree view. add key event handler for searchRequirementField. remove some import lines which are unused.

--- src/main/java/ir/salmanian/controllers/RequirementsController.java
+++ src/main/java/ir/salmanian/controllers/RequirementsController.java
@@ -1,52 +1,56 @@
 package ir.salmanian.controllers;
 
-import ir.salmanian.models.Priority;
 import ir.salmanian.models.Requirement;
 import ir.salmanian.services.RequirementService;
 import ir.salmanian.utils.ProjectHolder;
 import ir.salmanian.utils.RequirementHolder;
-import javafx.beans.property.*;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventDispatchChain;
+import javafx.event.EventDispatcher;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.CheckBoxTreeCell;
+import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeCell;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import org.apache.commons.lang.WordUtils;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class RequirementsController implements Initializable {
-    @FXML
-    private TreeView<String> filterTreeView;
+
     @FXML
     private TextField searchRequirementField;
-    @FXML
-    private Button filterBtn;
+
     @FXML
     private TreeView<Requirement> requirementTreeView;
-    private Set<Requirement> selectedRequirements = new HashSet<>();
+    @FXML
+    private Button searchBtn;
     private ObservableList<Requirement> requirementObservableList;
-    private ChangeListener<Boolean> treeViewEventListener;
-    List<TreeItem<String>> filters = new ArrayList<>();
+    private ChangeListener<Boolean> treeViewChangeListener;
 
     public RequirementsController() {
-        TreeItem<String> rootItem = new TreeItem<>(Priority.getNickname());
-        rootItem.setExpanded(true);
-        for (int i = 0; i < Priority.values().length; i++) {
-            Priority[] arrays = Priority.values();
-            TreeItem<String> item = new TreeItem<>(arrays[i].label);
-            rootItem.getChildren().add(item);
-        }
-        filters.add(rootItem);
+
+        loadRequirements();
+    }
+
+    private void loadRequirements() {
         requirementObservableList = FXCollections.observableArrayList();
         List<Requirement> requirements = RequirementService.getInstance().getRequirements(ProjectHolder.getInstance().getProject());
         for (Requirement requirement : requirements) {
@@ -58,7 +62,7 @@ public class RequirementsController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        treeViewEventListener = new ChangeListener<Boolean>() {
+        treeViewChangeListener = new ChangeListener<Boolean>() {
             @Override
             public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
                 BooleanProperty booleanProperty = (BooleanProperty) observable;
@@ -69,17 +73,16 @@ public class RequirementsController implements Initializable {
                     for (Requirement child : childrenRequirements) {
                         TreeItem<Requirement> childItem = new TreeItem<>(child);
                         childItem.expandedProperty().addListener(this::changed);
-                        childItem.getChildren().add(new CheckBoxTreeItem<>());
+                        childItem.getChildren().add(new TreeItem<>());
                         parentItem.getChildren().add(childItem);
                     }
                 }
             }
         };
 
-
         prepareTreeView();
-
-        requirementTreeView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        requirementTreeView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        /*requirementTreeView.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 if (event.getClickCount() == 2) {
@@ -93,16 +96,13 @@ public class RequirementsController implements Initializable {
                         e.printStackTrace();
                     }
 
+
+                } else if (event.isControlDown()) {
+                    requirementTreeView.refresh();
                 }
             }
-        });
+        });*/
 
-        TreeItem<String> r = new TreeItem<String>();
-        r.getChildren().addAll(filters);
-
-        filterTreeView.setRoot(r);
-        filterTreeView.setShowRoot(false);
-        filterTreeView.setCellFactory(param -> new CheckBoxTreeCell<>());
         requirementTreeView.setCellFactory(param -> {
             TreeCell<Requirement> treeCell = new TreeCell<Requirement>() {
                 @Override
@@ -110,6 +110,30 @@ public class RequirementsController implements Initializable {
                     super.updateItem(item, empty);
                     if (item != null && !empty) {
                         setText(item.toString());
+                        updateTreeItem(getTreeItem());
+                        EventDispatcher eventDispatcher = getEventDispatcher();
+                        setEventDispatcher(new EventDispatcher() {
+                            @Override
+                            public Event dispatchEvent(Event event, EventDispatchChain tail) {
+                                if (event instanceof MouseEvent) {
+                                    if (((MouseEvent) event).getClickCount() == 2) {
+                                        if (!event.isConsumed()) {
+                                            Requirement requirement = requirementTreeView.getSelectionModel().getSelectedItem().getValue();
+                                            RequirementHolder.getInstance().setRequirement(requirement);
+                                            Stage stage = ScreenController.getInstance().openNewStage(String.format("requirementFormStage-%d", requirement.getId()));
+                                            try {
+                                                ScreenController.getInstance().addScene(String.format("requirementFormScene-%d", requirement.getId()), "RequirementForm.fxml");
+                                                ScreenController.getInstance().activateScene(String.format("requirementFormScene-%d", requirement.getId()), stage);
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                        event.consume();
+                                    }
+                                }
+                                return eventDispatcher.dispatchEvent(event, tail);
+                            }
+                        });
                     } else
                         setText("");
                 }
@@ -120,10 +144,25 @@ public class RequirementsController implements Initializable {
                 public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                     String text = treeCell.getText();
                     text = text.replace("\n", " ");
-                    treeCell.setText(WordUtils.wrap(text, (int) (70 * (treeCell.getWidth() / 478)), "\n", false));
+                    treeCell.setText(WordUtils.wrap(text, (int) (70 * (treeCell.getWidth() / 478)), "\n", true));
                 }
             });
             return treeCell;
+        });
+        searchRequirementField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()){
+                    case ENTER:
+                        searchBtn.fire();
+                        event.consume();
+                        break;
+                    case TAB:
+                        searchBtn.requestFocus();
+                        event.consume();
+                        break;
+                }
+            }
         });
     }
 
@@ -132,16 +171,59 @@ public class RequirementsController implements Initializable {
         for (Requirement req : requirementObservableList) {
             TreeItem<Requirement> item = new TreeItem<>(req);
             item.getChildren().add(new TreeItem<>());
-            item.expandedProperty().addListener(treeViewEventListener);
+            item.expandedProperty().addListener(treeViewChangeListener);
             rootItem.getChildren().add(item);
         }
         requirementTreeView.setRoot(rootItem);
         requirementTreeView.setShowRoot(false);
     }
 
+    private void refreshTreeItem(TreeItem<Requirement> root) {
+        List<TreeItem<Requirement>> removeList = new ArrayList<>();
+        List<Requirement> children;
+        if (root.getValue() != null) {
+            children = RequirementService.getInstance().getChildrenRequirements(root.getValue());
+        } else {
+            search();
+            children = requirementObservableList;
+        }
+        for (TreeItem<Requirement> childItem : root.getChildren()) {
+            if (children.contains(childItem.getValue())) {
+                Requirement child = children.get(children.indexOf(childItem.getValue()));
+                children.remove(child);
+                childItem.setValue(child);
+                if (childItem.isExpanded()) {
+                    refreshTreeItem(childItem);
+                }
+            } else {
+                removeList.add(childItem);
+            }
+        }
+        for (TreeItem<Requirement> removeItem : removeList) {
+            root.getChildren().remove(removeItem);
+        }
+        for (Requirement child : children) {
+            TreeItem<Requirement> newChildItem = new TreeItem<>(child);
+            newChildItem.expandedProperty().addListener(treeViewChangeListener);
+            newChildItem.getChildren().add(new TreeItem<>());
+            root.getChildren().add(newChildItem);
+        }
+    }
+
+    public void refreshTreeView() {
+        refreshTreeItem(requirementTreeView.getRoot());
+    }
+
     @FXML
     public void onNewRequirementClick(ActionEvent event) throws IOException {
-        RequirementHolder.getInstance().setRequirement(new Requirement());
+        ObservableList<TreeItem<Requirement>> parentItems = requirementTreeView.getSelectionModel().getSelectedItems();
+        List<Requirement> parents = new ArrayList<>();
+        parentItems.forEach(parentItem -> {
+            parents.add(parentItem.getValue());
+        });
+        Requirement newRequirement = new Requirement();
+        newRequirement.setParents(parents);
+        RequirementHolder.getInstance().setRequirement(newRequirement);
         Stage stage = ScreenController.getInstance().openNewStage("newRequirementFormStage");
         if (stage.getScene() == null) {
             ScreenController.getInstance().addScene("newRequirementFormScene", "RequirementForm.fxml");
@@ -156,26 +238,25 @@ public class RequirementsController implements Initializable {
 
     @FXML
     public void onSearchClick(ActionEvent event) {
+        search();
+        prepareTreeView();
+    }
+
+    private void search() {
+        requirementObservableList.clear();
         if (searchRequirementField.getText().trim().isEmpty()) {
-            requirementObservableList.clear();
-            List<Requirement> requirements = RequirementService.getInstance().getRequirements(ProjectHolder.getInstance().getProject());
+            List<Requirement> requirements = RequirementService.getInstance()
+                    .getRequirements(ProjectHolder.getInstance().getProject());
             for (Requirement requirement : requirements) {
                 if (requirement.getLevel() == 1) {
                     requirementObservableList.add(requirement);
                 }
             }
-            prepareTreeView();
-            return;
-        }
-        List<Requirement> requirements = RequirementService.getInstance()
-                .searchRequirement(searchRequirementField.getText().trim(), ProjectHolder.getInstance().getProject());
-        requirementObservableList.clear();
-        requirementObservableList.addAll(requirements);
-        prepareTreeView();
-    }
+        } else {
+            List<Requirement> requirements = RequirementService.getInstance()
+                    .searchRequirement(searchRequirementField.getText().trim(), ProjectHolder.getInstance().getProject());
+            requirementObservableList.addAll(requirements);
 
-    @FXML
-    public void onFilterClick() {
-        System.out.println(filterTreeView.getSelectionModel().getSelectedItems());
+        }
     }
 }
