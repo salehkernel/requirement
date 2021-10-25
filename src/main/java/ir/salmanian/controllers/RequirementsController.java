f0281235d6b479edbabaf3415143111e1c60fa48
MHD <mt0132@gmail.com>
2021-10-25 12:52:01 +0330

    add a flow pane to tree cell which contains a label to show requirement prefix and title (toString method) and an image view to show whether it is user or system requirement. the label styleProperty is bound to the state of evaluation status to set the text fill color green if the requirement evaluation status is met and black otherwise. also the tree cell styleProperty is  bound to some dynamic styles(border color and border insets are according to requirement item level) and some static styles(5px border radius and 0px indent). for this purpose the required methods named "getTextFillStyle","getBorderColorStyle", "getBorderInsetsStyle" and "concatAllStyles" are created in anonymous class of tree cell. the tree cell graphic is set to the flow pane and the event dispatcher for double click is set for the flow pane. also the code is reformatted and a little is cleaned.

--- src/main/java/ir/salmanian/controllers/RequirementsController.java
+++ src/main/java/ir/salmanian/controllers/RequirementsController.java
@@ -18,20 +18,26 @@ import javafx.event.EventDispatcher;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import org.apache.commons.lang.WordUtils;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -79,38 +85,39 @@ public class RequirementsController implements Initializable {
 
         prepareTreeView();
         requirementTreeView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        /*requirementTreeView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                if (event.getClickCount() == 2) {
-                    try {
-                        Requirement requirement = requirementTreeView.getSelectionModel().getSelectedItem().getValue();
-                        RequirementHolder.getInstance().setRequirement(requirement);
-                        Stage stage = ScreenController.getInstance().openNewStage(String.format("requirementFormStage-%d", requirement.getId()));
-                        ScreenController.getInstance().addScene(String.format("requirementFormScene-%d", requirement.getId()), "RequirementForm.fxml");
-                        ScreenController.getInstance().activateScene(String.format("requirementFormScene-%d", requirement.getId()), stage);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-
-                } else if (event.isControlDown()) {
-                    requirementTreeView.refresh();
-                }
-            }
-        });*/
         requirementTreeView.setCellFactory(param -> {
             TreeCell<Requirement> treeCell = new TreeCell<Requirement>() {
+                FlowPane pane;
+                ImageView requirementIcon;
+                Label requirementTitleLabel;
+
                 @Override
                 public void updateItem(Requirement item, boolean empty) {
                     super.updateItem(item, empty);
+                    pane = new FlowPane(Orientation.HORIZONTAL, 15, 15);
+                    requirementIcon = new ImageView();
+                    requirementTitleLabel = new Label();
                     if (item != null && !empty) {
-                        setText(item.toString());
+                        requirementTitleLabel.setText(item.toString());
+                        requirementTitleLabel.setWrapText(true);
+                        try {
+                            requirementIcon.setImage(new Image(getClass()
+                                    .getResource(String.format("/img/%s.png", item.getLevel() == 1 ? "user" : "system"))
+                                    .toURI().toString()));
+                        } catch (URISyntaxException e) {
+                            e.printStackTrace();
+                        }
+                        pane.getChildren().add(requirementIcon);
+                        pane.getChildren().add(requirementTitleLabel);
+                        requirementTitleLabel.styleProperty().bind(Bindings.createStringBinding(
+                                () -> concatAllStyles(getTextFillStyle(item))));
                         styleProperty().bind(Bindings.createStringBinding(
-                                () -> String.format("-fx-text-fill: %s;", getMetColor(item))));
+                                () -> concatAllStyles(getBorderColorStyle(item), "-fx-border-radius: 5px;",
+                                        getBorderInsetsStyle(item), "-fx-indent: 0px;")
+                        ));
                         updateTreeItem(getTreeItem());
                         EventDispatcher eventDispatcher = getEventDispatcher();
-                        setEventDispatcher(new EventDispatcher() {
+                        pane.setEventDispatcher(new EventDispatcher() {
                             @Override
                             public Event dispatchEvent(Event event, EventDispatchChain tail) {
                                 if (event instanceof MouseEvent) {
@@ -124,12 +131,72 @@ public class RequirementsController implements Initializable {
                                 return eventDispatcher.dispatchEvent(event, tail);
                             }
                         });
-                    } else
+                        setGraphic(pane);
+                    } else {
                         setText("");
+                        setGraphic(null);
+                        styleProperty().bind(Bindings.createStringBinding(() -> ""));
+                    }
                 }
 
-                private String getMetColor(Requirement item) {
-                    return item.getEvaluationStatus() == EvaluationStatus.MET ? "#00FF00" : "#000000";
+                private String getBorderColorStyle(Requirement requirement) {
+                    String styleName = "-fx-border-color: ";
+                    String borderColor = "";
+                    switch (requirement.getLevel() % 10) {
+                        case 1:
+                            borderColor = "#6DA472";
+                            break;
+                        case 2:
+                            borderColor = "#0000FF";
+                            break;
+                        case 3:
+                            borderColor = "#D2B48C";
+                            break;
+                        case 4:
+                            borderColor = "#2E8B57";
+                            break;
+                        case 5:
+                            borderColor = "#8B4513";
+                            break;
+                        case 6:
+                            borderColor = "#B0E0E6";
+                            break;
+                        case 7:
+                            borderColor = "#00FFFF";
+                            break;
+                        case 8:
+                            borderColor = "#DEB887";
+                            break;
+                        case 9:
+                            borderColor = "#708090";
+                            break;
+                        case 0:
+                            borderColor = "#8A2BE2";
+                            break;
+
+                    }
+                    return String.format("%s%s;", styleName, borderColor);
+                }
+
+                private String getTextFillStyle(Requirement requirement) {
+                    String styleName = "-fx-text-fill: ";
+                    String fillColor = requirement.getEvaluationStatus() == EvaluationStatus.MET ? "green" : "black";
+                    return String.format("%s%s;", styleName, fillColor);
+                }
+
+                private String getBorderInsetsStyle(Requirement requirement) {
+                    String styleName = "-fx-border-insets: ";
+                    String value = String.format("1px 1px 1px %dpx", (requirement.getLevel() - 1) * 25 + 1);
+                    return String.format("%s%s;", styleName, value);
+                }
+
+                private String concatAllStyles(String... styles) {
+                    StringBuilder allStyles = new StringBuilder("");
+                    for (String style : styles) {
+                        if (style != null)
+                            allStyles.append(style);
+                    }
+                    return allStyles.toString();
                 }
             };
             treeCell.widthProperty().addListener(new ChangeListener<Number>() {
@@ -331,7 +398,7 @@ public class RequirementsController implements Initializable {
         boolean isNowExpanded = root.isExpanded();
         root.setExpanded(true);
         boolean contains = false;
-        for (TreeItem<Requirement> child: root.getChildren()) {
+        for (TreeItem<Requirement> child : root.getChildren()) {
             if (containsMetRequirement(child) || child.getValue().getEvaluationStatus() == EvaluationStatus.MET) {
                 contains = true;
             }
