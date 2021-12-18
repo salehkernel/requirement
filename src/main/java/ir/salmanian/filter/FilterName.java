cb2c86843cdcda3f51003ff705b2a83c4c3bbcd6
yasin <mt0132@gmail.com>
2021-12-18 10:52:35 +0330

    FilterName class is created which includes name of filters as static strings which should be shown in the ui.

---
+++ src/main/java/ir/salmanian/filter/FilterName.java
@@ -0,0 +1,11 @@
+package ir.salmanian.filter;
+
+public class FilterName {
+    public static final String PRIORITY = "اولویت";
+    public static final String REQUIREMENT_TYPE = "نوع نیازمندی";
+    public static final String CHANGE = "تغییرات";
+    public static final String REVIEW_STATUS = "وضعیت بازبینی";
+    public static final String EVALUATION_STATUS = "وضعیت ارزیابی";
+    public static final String EVALUATION_METHOD = "روش ارزیابی";
+    public static final String QUALITY_FACTOR = "فاکتور کیفی";
+}
