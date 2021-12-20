8f936f93ae2da1e00dacb3522a4f20c8529cf7de
yasin <mt0132@gmail.com>
2021-12-20 10:44:17 +0330

    some elements are added to handle embedded or external databases. the onEmbeddedDBClick, onExternalDBClick, onDBTestClick, onConfirmClick, connectionPaneFieldsAreOK methods are added for handling connection to different databases.

--- src/main/java/ir/salmanian/controllers/LoginController.java
+++ src/main/java/ir/salmanian/controllers/LoginController.java
@@ -1,17 +1,24 @@
 package ir.salmanian.controllers;
 
+import ir.salmanian.db.DBMS;
+import ir.salmanian.db.DatabaseConfig;
+import ir.salmanian.db.DatabaseManagement;
 import ir.salmanian.services.UserService;
 import ir.salmanian.utils.UserHolder;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import org.hibernate.SessionFactory;
 
 import java.io.IOException;
 import java.net.URL;
@@ -29,29 +36,99 @@ public class LoginController implements Initializable {
     @FXML
     private Label errorLabel;
     @FXML
+    private RadioButton embeddedDBRadioBtn;
+    @FXML
+    private RadioButton externalDBRadioBtn;
+    @FXML
+    private Button confirmBtn;
+    @FXML
+    private ComboBox<DBMS> dbTypeCombo;
+    @FXML
+    private TextField hostField;
+    @FXML
+    private TextField portField;
+    @FXML
+    private TextField dbNameField;
+    @FXML
+    private TextField dbUsernameField;
+    @FXML
+    private TextField dbPasswordField;
+    @FXML
+    private Label connectionLabel;
+    @FXML
+    private Button dbTestBtn;
+    @FXML
     private AnchorPane loginPane;
+    @FXML
+    private AnchorPane dbConnectionPane;
+
+    private ToggleGroup dbToggleGroup;
+    private ObservableList<DBMS> dbmsObservableList;
     private EventHandler<KeyEvent> defaultEnterKeyPressEventHandler;
+    private EventHandler<KeyEvent> defaultConnectionKeyPressEventHandler;
+    private boolean applicationStartup = true;
+
+    private DatabaseConfig dbConfig;
 
     public LoginController() {
+        dbConfig = DatabaseConfig.getInstance();
         defaultEnterKeyPressEventHandler = new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                if (event.getCode() == KeyCode.ENTER){
+                if (event.getCode() == KeyCode.ENTER) {
                     loginBtn.fire();
                     event.consume();
                 }
             }
         };
+        defaultConnectionKeyPressEventHandler = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER) {
+                    dbTestBtn.fire();
+                    event.consume();
+                }
+            }
+        };
+        dbToggleGroup = new ToggleGroup();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         String backgroundPath = getClass().getResource("/img/login.jpeg").toExternalForm();
         loginPane.setStyle("-fx-background-image: url(" + backgroundPath + ");-fx-background-size: cover;");
+
         usernameField.setOnKeyPressed(defaultEnterKeyPressEventHandler);
         passwordField.setOnKeyPressed(defaultEnterKeyPressEventHandler);
+
+        hostField.setOnKeyPressed(defaultConnectionKeyPressEventHandler);
+        portField.setOnKeyPressed(defaultConnectionKeyPressEventHandler);
+        dbNameField.setOnKeyPressed(defaultConnectionKeyPressEventHandler);
+        dbUsernameField.setOnKeyPressed(defaultConnectionKeyPressEventHandler);
+        dbPasswordField.setOnKeyPressed(defaultConnectionKeyPressEventHandler);
+
+        embeddedDBRadioBtn.setToggleGroup(dbToggleGroup);
+        externalDBRadioBtn.setToggleGroup(dbToggleGroup);
+        dbToggleGroup.selectToggle(embeddedDBRadioBtn);
+        confirmBtn.fire();
+
+        dbmsObservableList = FXCollections.observableArrayList(DBMS.values());
+        dbTypeCombo.setItems(dbmsObservableList);
+        dbTypeCombo.getSelectionModel().select(0);
+
+        portField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    portField.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        applicationStartup = false;
     }
 
+    @FXML
     public void onRegisterPageClick() throws IOException {
         ScreenController.getInstance().addScene("registerScene", "Register.fxml");
         ScreenController.getInstance().activateScene("registerScene", ScreenController.getInstance().getMainStage());
@@ -61,11 +138,76 @@ public class LoginController implements Initializable {
         usernameField.requestFocus();
     }
 
+    @FXML
     public void onLoginClick() throws IOException {
         if (!checkEmptyFields() && login()) {
             UserHolder.getInstance().setUser(UserService.getInstance().getUserByUsername(usernameField.getText()));
             ScreenController.getInstance().addScene("projectsScene", "Projects.fxml");
             ScreenController.getInstance().activateScene("projectsScene", ScreenController.getInstance().getMainStage());
+            errorLabel.setText("");
+            connectionLabel.setText("");
+            usernameField.clear();
+            passwordField.clear();
+            usernameField.requestFocus();
+        }
+    }
+
+    @FXML
+    public void onEmbeddedDBClick(ActionEvent event) {
+        dbConnectionPane.setVisible(false);
+    }
+
+    @FXML
+    public void onExternalDBClick(ActionEvent event) {
+        dbConnectionPane.setVisible(true);
+    }
+
+    @FXML
+    public void onDBTestClick(ActionEvent event) {
+        if (connectionPaneFieldsAreOK()) {
+            DBMS dbms = dbTypeCombo.getValue();
+            String hostAddress = hostField.getText().trim();
+            int port = Integer.parseInt(portField.getText().trim());
+            String dbName = dbNameField.getText().trim();
+            String username = dbUsernameField.getText().trim();
+            String password = dbPasswordField.getText().trim();
+            if (dbConfig.testConnection(dbms, hostAddress, port, dbName, username, password)) {
+                connectionLabel.setText("تست موفق");
+                connectionLabel.setTextFill(Color.GREEN);
+            } else {
+                connectionLabel.setText("خطا در برقراری ارتباط");
+                connectionLabel.setTextFill(Color.RED);
+            }
+        }
+    }
+
+    @FXML
+    public void onConfirmClick(ActionEvent event) {
+        RadioButton selected = (RadioButton) dbToggleGroup.getSelectedToggle();
+        if (selected.equals(embeddedDBRadioBtn)) {
+            DatabaseManagement.getInstance().setSessionFactory(dbConfig.useEmbeddedDB());
+            if (!applicationStartup) {
+                connectionLabel.setText("پایگاه داده نرم افزار به پایگاه داده تعبیه شده تغییر یافت.");
+                connectionLabel.setTextFill(Color.GREEN);
+            }
+        } else {
+            if (connectionPaneFieldsAreOK()) {
+                DBMS dbms = dbTypeCombo.getValue();
+                String hostAddress = hostField.getText().trim();
+                int port = Integer.parseInt(portField.getText().trim());
+                String dbName = dbNameField.getText().trim();
+                String username = dbUsernameField.getText().trim();
+                String password = dbPasswordField.getText().trim();
+                SessionFactory sessionFactory = dbConfig.useExternalDB(dbms, hostAddress, port, dbName, username, password);
+                if (sessionFactory != null) {
+                    DatabaseManagement.getInstance().setSessionFactory(sessionFactory);
+                    connectionLabel.setText("پایگاه داده نرم افزار به پایگاه داده خارجی تغییر یافت.");
+                    connectionLabel.setTextFill(Color.GREEN);
+                } else {
+                    connectionLabel.setText("خطا در برقراری ارتباط");
+                    connectionLabel.setTextFill(Color.RED);
+                }
+            }
         }
     }
 
@@ -97,5 +239,36 @@ public class LoginController implements Initializable {
         }
         errorLabel.setText(message);
         return false;
+    }
+
+
+    private boolean connectionPaneFieldsAreOK() {
+        connectionLabel.setTextFill(Color.RED);
+        if (hostField.getText().trim().isEmpty()) {
+            connectionLabel.setText("آدرس میزبان نمی تواند خالی باشد.");
+            hostField.requestFocus();
+            return false;
+        }
+        if (portField.getText().trim().isEmpty()) {
+            connectionLabel.setText("پورت نمی تواند خالی باشد.");
+            portField.requestFocus();
+            return false;
+        }
+        if (dbNameField.getText().trim().isEmpty()) {
+            connectionLabel.setText("نام پایگاه داده نمی تواند خالی باشد.");
+            dbNameField.requestFocus();
+            return false;
+        }
+        if (dbUsernameField.getText().trim().isEmpty()) {
+            connectionLabel.setText("نام کاربری نمی تواند خالی باشد.");
+            dbUsernameField.requestFocus();
+            return false;
+        }
+        if (dbPasswordField.getText().trim().isEmpty()) {
+            connectionLabel.setText("رمز عبور نمی تواند خالی باشد.");
+            dbPasswordField.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
