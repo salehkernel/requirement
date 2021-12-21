c871d847b0f86962223928f2048fc18f48385778
yasin <mt0132@gmail.com>
2021-12-21 09:18:52 +0330

    add class level javadoc based comments for RequirementManagementApplication class.

--- src/main/java/ir/salmanian/RequirementManagementApplication.java
+++ src/main/java/ir/salmanian/RequirementManagementApplication.java
@@ -5,6 +5,10 @@ import ir.salmanian.db.DatabaseManagement;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * RequirementManagementApplication class is the main class of application which extends {@link Application} class.
+ * This class contains main method which launches the application using overridden start method in this class.
+ */
 public class RequirementManagementApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
